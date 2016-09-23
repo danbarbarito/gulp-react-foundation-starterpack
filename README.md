3cef0ab0ad520a6df7ed9bc5774327d4db8ff305
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-09-23 00:43:05 -0400

    :rocket:

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gulp-React-Foundation-Starterpack
+
+## Usage
+
+1. Run `npm install`
+2. Run `gulp`
+3. Celebrate.
---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+global.$ = global.jQuery = require('jquery');
+var Foundation = require('foundation-sites');
+
+import React from 'react';
+import ReactDOM from 'react-dom';

80e1e832b6812d62865d1808495a4db15a299dd1
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-09-23 00:47:53 -0400

    :rocket:

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
 global.$ = global.jQuery = require('jquery');
+
 var Foundation = require('foundation-sites');
+$(document).foundation();
 
 import React from 'react';
 import ReactDOM from 'react-dom';
